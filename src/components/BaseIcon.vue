61034f208c5bce1996e06c8d4db896e80e14c2f6
dehlix <deborahligorio@gmail.com>
2019-11-06 13:20:10 +0100

    global component

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,33 @@
+ <template>
+  <div class="icon-wrapper">
+    <svg class="icon" width="width" height="height">
+      <use v-bind="{ 'xlink:href': '/logo.svg#' +name }" />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    width: {
+      type: [Number, String],
+      default: 334
+    },
+    height: {
+      type: [Number, String],
+      default: 211
+    }
+  }
+};
+</script>
+
+<style scoped >
+.icon-wrapper {
+  display: inline-flex;
+  align-items: center;
+}
+.icon {
+  margin: 6px;
+}
+</style>
